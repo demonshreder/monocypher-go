a33e1ec8a7915ab011e5664bdd9c2e231883404b
Kamalavelan <sskamalavelan@gmail.com>
2017-08-22 13:37:15 +0530

    crypto_aead_lock and crypto_aead_unlock finished
    Signed-off-by: Kamalavelan <sskamalavelan@gmail.com>

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/demonshreder/monocypher-go/monocypher"
-)
+import "fmt"
+import "github.com/demonshreder/monocypher-go/monocypher"
 
 func main() {
-	key := "OMG kamal is here again touche. Such a sexy guy"
-	nonce := "This is kamal too, kek guys, hahahahahaha"
-	plain := "Hello this is Kamal"
-	mac, cipher := monocypher.Lock(plain, nonce, key)
+	key := []byte("OMG kamal is here again touche. Such a sexy guy")
+	nonce := []byte("This is kamal too, kek guys, hahahahahaha")
+	plain := []byte("Hello this is Kamal")
+	ad := []byte("")
+	mac, cipher, add := monocypher.AeadLock(plain, nonce, key, ad)
 	// fmt.Println(len(mac), len(cipher), len("Hello this is Kamal"))
-	decipher := monocypher.Unlock(mac, cipher, nonce, key)
-	fmt.Println(len([]byte(key[:32])), []byte(key[:32]))
+	decipher := monocypher.AeadUnlock(mac, cipher, nonce, key, add)
+	// fmt.Println(len([]byte(key[:32])), []byte(key[:32]))
 	fmt.Println(len(plain), plain, len(decipher), string(decipher))
 }
--- monocypher/monocypher.go
+++ monocypher/monocypher.go
@@ -5,21 +5,13 @@ package monocypher
 // #include <stdio.h>
 // #include <stdlib.h>
 import "C"
-import "fmt"
+
 import "unsafe"
 
-// func main() {
-// 	cs := C.CString("cool")
-// 	defer C.free(unsafe.Pointer(cs))
-// }
-
-//Cool is cooler than the coolest
-func Cool() {
-	fmt.Println("standard shit, move on")
-}
-
 // Lock is authenticated encryption using XChacha20 & Poly1305
-func Lock(plaintext, nonce, key string) (mac, ciphertext []byte) {
+// Lock takes plaintext, nonce and key, returns the mac and the
+// ciphertext
+func Lock(plaintext, nonce, key []byte) (mac, ciphertext []byte) {
 	// func Lock(plaintext, nonce, key string) (mac, ciphertext string) {
 	// void crypto_lock(uint8_t        mac[16],
 	// uint8_t       *ciphertext,
@@ -40,15 +32,16 @@ func Lock(plaintext, nonce, key string) (mac, ciphertext []byte) {
 	defer C.free(unsafe.Pointer(Ccipher))
 	//	C Method call
 	C.crypto_lock(Cmac, Ccipher, Ckey, Cnonce, Cplain, Csize)
-	// fmt.Println(&Ccipher, &Cmac)
 	// Converting CTypes back to Go
 	var Ncipher []byte = C.GoBytes(unsafe.Pointer(Ccipher), C.int(len(plaintext)))
 	var Nmac []byte = C.GoBytes(unsafe.Pointer(Cmac), C.int(16))
 	return Nmac, Ncipher
 }
 
-// Unlock decrypts the message back into plaintext
-func Unlock(mac, ciphertext []byte, nonce, key string) (plaintext []byte) {
+// Unlock decrypts the ciphertext from Lock. It first checks the integrity
+// using mac, then uses the same nonce and key to decrypt the cipher and
+// returns the plaintext in bytes.
+func Unlock(mac, ciphertext, nonce, key []byte) (plaintext []byte) {
 	// int crypto_unlock(uint8_t       *plaintext,
 	// const uint8_t  key[32],
 	// const uint8_t  nonce[24],
@@ -72,121 +65,163 @@ func Unlock(mac, ciphertext []byte, nonce, key string) (plaintext []byte) {
 	// fmt.Println(Cplain)
 	// // Converting CTypes back to Go
 	// var Nplain []byte = C.GoBytes(unsafe.Pointer(Cplain), C.int(len(ciphertext)))
-	var Nmac []byte = C.GoBytes(unsafe.Pointer(Ckey), C.int(32))
-	fmt.Println(len(Nmac), Nmac)
+	// var Nmac []byte = C.GoBytes(unsafe.Pointer(Ckey), C.int(32))
+	// fmt.Println(len(Nmac), Nmac)
 	var Nplain []byte = C.GoBytes(unsafe.Pointer(Cplain), C.int(len(ciphertext)))
 	// return Nmac, Ncipher
 
 	return Nplain
 }
 
-func Crypto_aead_lock() {
+// AeadLock allows some additional data to be signed though not
+// encrypted with the rest. AeadLock returns mac, ciphertext and the authenticated
+// text.
+func AeadLock(plaintext, nonce, key, addData []byte) (mac, ciphertext, data []byte) {
 	// void crypto_aead_lock(uint8_t        mac[16],
 	// uint8_t       *ciphertext,
 	// const uint8_t  key[32],
 	// const uint8_t  nonce[24],
 	// const uint8_t *ad       , size_t ad_size,
 	// const uint8_t *plaintext, size_t text_size);
+
+	CadSize := (C.size_t)(len(addData))
+	Cad := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(addData))))
+	defer C.free(unsafe.Pointer(Cad))
+	CtextSize := (C.size_t)(len(plaintext))
+	Cplain := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(plaintext))))
+	defer C.free(unsafe.Pointer(Cplain))
+	Ckey := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(key[:32]))))
+	defer C.free(unsafe.Pointer(Ckey))
+	Cnonce := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(nonce[:24]))))
+	defer C.free(unsafe.Pointer(Cnonce))
+	Cmac := (*C.uint8_t)(unsafe.Pointer(C.CBytes(make([]uint8, 16))))
+	defer C.free(unsafe.Pointer(Cmac))
+	Ccipher := (*C.uint8_t)(unsafe.Pointer(C.CBytes(make([]uint8, len(plaintext)))))
+	defer C.free(unsafe.Pointer(Ccipher))
+	//	C Method call
+	C.crypto_aead_lock(Cmac, Ccipher, Ckey, Cnonce, Cad, CadSize, Cplain, CtextSize)
+	// Converting CTypes back to Go
+	var Ncipher []byte = C.GoBytes(unsafe.Pointer(Ccipher), C.int(len(plaintext)))
+	var Nmac []byte = C.GoBytes(unsafe.Pointer(Cmac), C.int(16))
+	var Nad []byte = C.GoBytes(unsafe.Pointer(Cad), C.int(CadSize))
+	return Nmac, Ncipher, Nad
 }
 
-func Crypto_aead_unlock() {
+// AeadUnlock takes mac, ciphertext, nonce, key and authenticated data, returns
+// the deciphered plaintext.
+func AeadUnlock(mac, ciphertext, nonce, key, addData []byte) (plaintext []byte) {
 	// int crypto_aead_unlock(uint8_t       *plaintext,
 	// const uint8_t  key[32],
 	// const uint8_t  nonce[24],
 	// const uint8_t  mac[16],
 	// const uint8_t *ad        , size_t ad_size,
 	// const uint8_t *ciphertext, size_t text_size);
+
+	CadSize := (C.size_t)(len(addData))
+	Cad := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(addData))))
+	defer C.free(unsafe.Pointer(Cad))
+	Csize := (C.size_t)(len(ciphertext))
+	Ccipher := (*C.uint8_t)(unsafe.Pointer(C.CBytes(ciphertext)))
+	defer C.free(unsafe.Pointer(Ccipher))
+	Ckey := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(key[:32]))))
+	defer C.free(unsafe.Pointer(Ckey))
+	Cnonce := (*C.uint8_t)(unsafe.Pointer(C.CBytes([]uint8(nonce[:24]))))
+	defer C.free(unsafe.Pointer(Cnonce))
+	Cmac := (*C.uint8_t)(unsafe.Pointer(C.CBytes(mac)))
+	defer C.free(unsafe.Pointer(Cmac))
+	Cplain := (*C.uint8_t)(unsafe.Pointer(C.CBytes(make([]uint8, len(ciphertext)))))
+	defer C.free(unsafe.Pointer(Cplain))
+	//	C Method call
+	C.crypto_aead_unlock(Cplain, Ckey, Cnonce, Cmac, Cad, CadSize, Ccipher, Csize)
+	// Converting CTypes back to Go
+	var Nplain []byte = C.GoBytes(unsafe.Pointer(Cplain), C.int(len(ciphertext)))
+	// return Nmac, Ncipher
+
+	return Nplain
 }
 
-func Crypto_key_exchange() {
+func crypto_sign() {
+	// void crypto_sign(uint8_t        signature[64],
+	// const uint8_t  secret_key[32],
+	// const uint8_t  public_key[32], // optional, may be null
+	// const uint8_t *message, size_t message_size);
+
+}
+
+func crypto_check() {
+	// int crypto_check(const uint8_t  signature[64],
+	// const uint8_t  public_key[32],
+	// const uint8_t *message, size_t message_size);
+}
+
+func crypto_key_exchange() {
 	// int crypto_key_exchange(uint8_t       shared_key      [32],
 	// const uint8_t your_secret_key [32],
 	// const uint8_t their_public_key[32]);
 }
-func Crypto_x25519_public_key() {
+func crypto_x25519_public_key() {
 	// void crypto_x25519_public_key(uint8_t       public_key[32],
 	// const uint8_t secret_key[32]);
 
 }
 
-func Crypto_x25519() {
+func crypto_x25519() {
 	// int  crypto_x25519(uint8_t       shared_secret   [32],
 	// const uint8_t your_secret_key [32],
 	// const uint8_t their_public_key[32]);
 }
 
-func Crypto_sign_public_key() {
+func crypto_sign_public_key() {
 	// void crypto_sign_public_key(uint8_t        public_key[32],
 	// const uint8_t  secret_key[32]);
 
 }
 
-func Crypto_sign() {
-	// void crypto_sign(uint8_t        signature[64],
-	// const uint8_t  secret_key[32],
-	// const uint8_t  public_key[32], // optional, may be null
-	// const uint8_t *message, size_t message_size);
-}
-
-func Crypto_check() {
-	// int crypto_check(const uint8_t  signature[64],
-	// const uint8_t  public_key[32],
-	// const uint8_t *message, size_t message_size);
-}
-func Crypto_blake2b_general() {
+func crypto_blake2b_general() {
 
 	// void crypto_blake2b_general(uint8_t       *digest, size_t digest_size,
 	// const uint8_t *key   , size_t key_size,
 	// const uint8_t *in    , size_t in_size);
 }
-func Crypto_blake2b() {
+func crypto_blake2b() {
 	// void crypto_blake2b(uint8_t digest[64], const uint8_t *in, size_t in_size);
 }
 
-func Crypto_blake2b_general_init() {
+func crypto_blake2b_general_init() {
 	// void crypto_blake2b_general_init(crypto_blake2b_ctx *ctx, size_t digest_size,
 	// const uint8_t      *key, size_t key_size);
 
 }
-func Crypto_blake2b_init() {
+func crypto_blake2b_init() {
 	// void crypto_blake2b_init(crypto_blake2b_ctx *ctx);
 }
 
-func Crypto_blake2b_update() {
+func crypto_blake2b_update() {
 	// void crypto_blake2b_update(crypto_blake2b_ctx *ctx,
 	// const uint8_t      *in, size_t in_size);
 }
 
-func Crypto_blake2b_final() {
+func crypto_blake2b_final() {
 	// void crypto_blake2b_final(crypto_blake2b_ctx *ctx, uint8_t *digest);
 }
-func Crypto_argon2i() {
-	// void crypto_argon2i(uint8_t       *tag,       uint32_t tag_size,
-	// void          *work_area, uint32_t nb_blocks,
-	// uint32_t       nb_iterations,
-	// const uint8_t *password,  uint32_t password_size,
-	// const uint8_t *salt,      uint32_t salt_size,
-	// const uint8_t *key,       uint32_t key_size,
-	// const uint8_t *ad,        uint32_t ad_size);
-}
-func Crypto_memcmp() {
+func crypto_memcmp() {
 	// int crypto_memcmp (const uint8_t *p1, const uint8_t *p2, size_t n);
 }
-func Crypto_zerocmp() {
+func crypto_zerocmp() {
 	// int crypto_zerocmp(const uint8_t *p , size_t n);
 }
-func Crypto_chacha20_H() {
+func crypto_chacha20_H() {
 	// void crypto_chacha20_H(uint8_t       out[32],
 	// const uint8_t key[32],
 	// const uint8_t in [16]);
 }
 
-func Crypto_chacha20_init() {
+func crypto_chacha20_init() {
 	// void crypto_chacha20_init(crypto_chacha_ctx *ctx,
 	// const uint8_t      key[32],
 	// const uint8_t      nonce[8]);
 }
-func Crypto_chacha20_Xinit() {
+func crypto_chacha20_Xinit() {
 	// void crypto_chacha20_Xinit(crypto_chacha_ctx *ctx,
 	// const uint8_t      key[32],
 	// const uint8_t      nonce[24]);
@@ -197,28 +232,37 @@ func crypto_chacha20_encrypt() {
 	// const uint8_t     *plain_text,
 	// size_t             message_size);
 }
-func Crypto_chacha20_stream() {
+func crypto_chacha20_stream() {
 	// void crypto_chacha20_stream(crypto_chacha_ctx *ctx,
 	// uint8_t           *cipher_text,
 	// size_t             message_size);
 }
-func Crypto_chacha20_set_ctr() {
+func crypto_chacha20_set_ctr() {
 	// void crypto_chacha20_set_ctr(crypto_chacha_ctx *ctx, uint64_t ctr);
 }
-func Crypto_poly1305_auth() {
+func crypto_poly1305_auth() {
 	// void crypto_poly1305_auth(uint8_t        mac[16],
 	// const uint8_t *m,
 	// size_t         msg_size,
 	// const uint8_t  key[32]);
 
 }
-func Crypto_poly1305_init() {
+func crypto_poly1305_init() {
 	// void crypto_poly1305_init(crypto_poly1305_ctx *ctx, const uint8_t key[32]);
 }
-func Crypto_poly1305_update() {
+func crypto_poly1305_update() {
 	// void crypto_poly1305_update(crypto_poly1305_ctx *ctx,
 	// const uint8_t *m, size_t bytes);
 }
-func Crypto_poly1305_final() {
+func crypto_poly1305_final() {
 	// void crypto_poly1305_final(crypto_poly1305_ctx *ctx, uint8_t mac[16]);
+}
+func crypto_argon2i() {
+	// void crypto_argon2i(uint8_t       *tag,       uint32_t tag_size,
+	// void          *work_area, uint32_t nb_blocks,
+	// uint32_t       nb_iterations,
+	// const uint8_t *password,  uint32_t password_size,
+	// const uint8_t *salt,      uint32_t salt_size,
+	// const uint8_t *key,       uint32_t key_size,
+	// const uint8_t *ad,        uint32_t ad_size);
 }
